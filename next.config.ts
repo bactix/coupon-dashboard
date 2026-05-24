5da01c047a35b9817126bb60ad005307faf6233a
mohammedhaydar97 <mohammedhaydar97@gmail.com>
2026-05-24 10:42:52 +0300

    fixed nextjs conf

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  outputFileTracingRoot: "C:/Users/ProUser/OneDrive/Desktop/coupon-dashboard",
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
